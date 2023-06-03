1f30a5763053b97dde2dd5bdfab9c318b910484b
leonardomartindev <leonardomartin.dev@gmail.com>
2023-06-03 19:56:17 -0300

    function calculator

--- script.js
+++ script.js
@@ -1,29 +1,99 @@
-//Toggle theme
+const normalTable = document.querySelector("table")
+const trs = normalTable.querySelectorAll("tr")
+const tdsNumber = document.querySelectorAll("number")
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+// console.log(tdsNumber)
 
-const themeToggle = document.querySelector('.theme-toggle');
-const iIcon = themeToggle.querySelector('div').querySelector("i")
-const iButton = themeToggle.querySelector('div')
-const html = document.querySelector("html")
-console.log(html)
-
-
-themeToggle.addEventListener('click', function() {
-    themeToggle.classList.toggle('dark');
-    iIcon.classList.toggle('fa-moon')
-    iButton.classList.toggle('cinza')
+trs.forEach((linha)=>{
+    const celulas = linha.querySelectorAll('td')
     
-    html.classList.toggle('dark-theme')
-});
-
-//Change type calculator
-
-const normalCalculator = document.querySelector(".container-normal-calculator")
-const scientificCalculator = document.querySelector(".container-scientific-calculator")
-
-const iconCalculator = document.querySelector(".scientific-icon")
-
-iconCalculator.addEventListener("click", ()=> {
-    normalCalculator.classList.toggle("hide")
-    scientificCalculator.classList.toggle("hide")
-    iconCalculator.classList.toggle('scientific')
+    celulas.forEach((tds) =>{
+        if(tds.classList.contains("number")) {
+            return tds.addEventListener("click", ()=>{
+                textArea.value += tds.innerText
+            })
+        }
+        if (tds.classList.contains("mult")) {
+            return tds.addEventListener("click", () => {
+              if (textArea.value) {
+                const lastChar = textArea.value.slice(-1);
+                if (lastChar !== "x" && lastChar !== "+" && lastChar !== "-" && lastChar !== "/") {
+                  textArea.value += "*";
+                }
+              }
+            });
+          }
+        if (tds.classList.contains("div")) {
+            return tds.addEventListener("click", () => {
+              if (textArea.value) {
+                const lastChar = textArea.value.slice(-1);
+                if (lastChar !== "x" && lastChar !== "+" && lastChar !== "-" && lastChar !== "/") {
+                  textArea.value += "/";
+                }
+              }
+            });
+          }
+          if (tds.classList.contains("sub")) {
+            return tds.addEventListener("click", () => {
+              if (textArea.value) {
+                const lastChar = textArea.value.slice(-1);
+                if (lastChar !== "x" && lastChar !== "+" && lastChar !== "-" && lastChar !== "/") {
+                  textArea.value += "-";
+                }
+              }
+            });
+          }
+          if (tds.classList.contains("soma")) {
+            return tds.addEventListener("click", () => {
+              if (textArea.value) {
+                const lastChar = textArea.value.slice(-1);
+                if (lastChar !== "x" && lastChar !== "+" && lastChar !== "-" && lastChar !== "/") {
+                  textArea.value += "+";
+                }
+              }
+            });
+          }
+        if(tds.classList.contains("ac")){
+            return tds.addEventListener("click", ()=>{
+                textArea.value = ""
+            })
+        }
+        if(tds.classList.contains("del")){
+            return tds.addEventListener("click", ()=>{
+                textArea.value = textArea.value.slice(0, -1)
+            })
+        }
+        if (tds.classList.contains("virgula")) {
+            return tds.addEventListener("click", () => {
+              if (!textArea.value) {
+                textArea.value = "0";
+              }
+              const lastChar = textArea.value.slice(-1);
+              if (
+                lastChar !== "x" &&
+                lastChar !== "+" &&
+                lastChar !== "-" &&
+                lastChar !== "/" &&
+                lastChar !== "," &&
+                !textArea.value.includes(",")
+              ) {
+                textArea.value += ",";
+              }
+            });
+          }
+          if(tds.classList.contains("equal")){
+            return tds.addEventListener("click", ()=>{
+                if(!textArea.value){
+                    window.alert("É necessário inserir uma expressão matemática válida")
+                    return
+                }
+                const resultado = eval(textArea.value)
+                textArea.value = resultado
+            })
+        }
+    })
 })
+
+
+const textArea = document.querySelector("textarea")
+textArea.readOnly = true;
